3e565d29696831e7c1249db04599b0a2d06bc38a
zhaopengfei <zpf@zhaopengfeideMacBook-Pro.local>
2019-05-13 18:12:01 +0800

    commit

--- example2/client.py
+++ example2/client.py
@@ -4,15 +4,17 @@ import grpc
 import data_pb2
 import data_pb2_grpc
 
+ADDRESS = "127.0.0.1:8989"
 
-def run():
-    channel = grpc.insecure_channel("127.0.0.1:8989")
+
+def run_client():
+    channel = grpc.insecure_channel(ADDRESS)
     client = data_pb2_grpc.FormatDataStub(channel)
 
-    for i in range(10):
+    for _ in range(10):
         response = client.doFormat(data_pb2.Data(test="hello world"))
         print("Result:", response.test)
 
 
 if __name__ == "__main__":
-    run()
+    run_client()
--- example2/server.py
+++ example2/server.py
@@ -13,16 +13,23 @@ class doFormat(data_pb2_grpc.FormatDataServicer):
         return data_pb2.Data(test=text.upper())
 
 
-def run():
+def run_server():
+    """
+    启动RPC服务
+    """
+    address = '127.0.0.1:8989'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     servicer = doFormat()
     data_pb2_grpc.add_FormatDataServicer_to_server(servicer, server)
-    server.add_insecure_port('127.0.0.1:8989')
+    server.add_insecure_port(address)
+    print(f"RPC SERVER Start {address}")
     server.start()
 
-    time.sleep(500)
-    server.stop(0)
+    try:
+        time.sleep(500)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 
 if __name__ == "__main__":
-    run()
+    run_server()
